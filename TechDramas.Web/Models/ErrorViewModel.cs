452e4838747230851d73704de501df53f432c6d5
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-08-09 20:17:34 -0300

    Initialized project

---
+++ TechDramas.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechDramas.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
