fe4715323a0dd8c06c92bccc139b086cf5300ded
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-08-09 20:52:16 -0300

    Created Db context

---
+++ TechDramas.Web/Data/TechDramasDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechDramas.Web.Models.Domain;
+
+namespace TechDramas.Web.Data
+{
+    public class TechDramasDbContext : DbContext
+    {
+        // Here, the constructor receives the db options from Program.cs and sets it to base's constructor (DbContext)
+        public TechDramasDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
