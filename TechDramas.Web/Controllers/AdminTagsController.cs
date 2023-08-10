20833ad0a0991114984abed66e10c0ef9e326db2
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-08-10 18:48:38 -0300

    Created new contoller and view for AdminTags

---
+++ TechDramas.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechDramas.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
